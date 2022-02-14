52817a73d928014c8ac58363db6fe4dc4bbe9f3a
Shari <sun.shari09@gmail.com>
2022-02-13 19:07:36 -0500

    update

--- main.py
+++ main.py
@@ -18,6 +18,3 @@ def get_photo():
     data = request.get_json().values()
     img = op.get_text_img(*data, ROTATION=(random.random() - 1) * 0.5)
     return send_file(io.BytesIO(img), mimetype='image/jpg')
-
-print('log: starting server')
-app.run(host='0.0.0.0', port=8080)
